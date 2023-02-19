03e910f635f1dea2b6080daea06bd4eb24b552a5
Nestor <nestor.is.a.developer@gmail.com>
2023-02-18 20:38:20 -0500

    Add react components

--- src/pages/NewPassword.jsx
+++ src/pages/NewPassword.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import '../styles/Login.scss';
+import '../styles/NewPassword.scss';
 
-const Login = () => {
+const NewPassword = () => {
 	return (
-		<div className="login">
-			<div className="form-container">
+		<div className="NewPassword">
+			<div className="NewPassword-container">
 				<img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
 				<h1 className="title">Create a new password</h1>
 				<p className="subtitle">Enter a new passwrd for yue account</p>
@@ -14,10 +14,10 @@ const Login = () => {
 					<label for="new-password" className="label">Password</label>
 					<input type="password" id="new-password" placeholder="*********" className="input input-password" />
 					<input type="submit" value="Confirm" className="primary-button login-button" />
-                </form>
-            </div>
-        </div>
+				</form>
+			</div>
+		</div>
 	);
 }
 
-export default Login;
+export default NewPassword;
