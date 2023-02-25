7099926f4dc8403ba9f8019651bd771a4b3b7743
Nestor <nestor.is.a.developer@gmail.com>
2023-02-25 15:49:03 -0500

    add hooks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import '@styles/Header.scss';
+import Menu from '@components/Menu';
+import MyOrder from '@containers/MyOrder';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
+import AppContext from '@context/AppContext';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
 
 const Header = () => {
+    const [toggle, setToggle] = useState(false);
+	const [toggleOrders, setToggleOrders] = useState(false);
+
+	//Lo que se haga en el product item va a estar en el heaer porque lo agrego al contexto.
+	//Entonces cuando se agregue un producto al carrito desde el product item aparecera en el carrito que està en header
+	const { state } = useContext(AppContext);
+
+    const handleToggle = () => {
+        setToggle(!toggle);
+    }
+
 	return (
 		<nav>
 			<img src={menu} alt="menu" className="menu" />
@@ -33,13 +47,17 @@ const Header = () => {
 			</div>
 			<div className="navbar-right">
 				<ul>
-					<li className="navbar-email">nestor@example.com</li>
-					<li className="navbar-shopping-cart">
+					<li className="navbar-email" onClick={handleToggle}>
+                        nestor@example.com
+                    </li>
+					<li className="navbar-shopping-cart" onClick={() => setToggleOrders(!toggleOrders)}>
 						<img src={shoppingCart} alt="shopping cart" />
-						<div>2</div>
+						{state.cart.length > 0 ? <div>{state.cart.length}</div> : null }
 					</li>
 				</ul>
 			</div>
+            {toggle && <Menu />}
+			{toggleOrders && <MyOrder />}
 		</nav>
 	);
 }
--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ProductItem from '../components/ProductItem';
+import useGetProducts from '@hooks/useGetProducts';
 import '../styles/ProductList.scss';
 
+const API = 'https://api.escuelajs.co/api/v1/products';
+
 const ProductList = () => {
+	const products = useGetProducts(API);
+
+	//key es porque react necesita un key para cada elemento del DOm que se va creando dinámicamente
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(product => (
+					<ProductItem product={product} key={product.id}/>
+				))}
 			</div>
 		</section>
 	);
