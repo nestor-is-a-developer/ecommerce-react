80e7947447858cfcbe8ee09e7579ed7c4be2c1cf
Nestor <nestor.is.a.developer@gmail.com>
2023-02-25 19:42:46 -0500

    configs related to githubpages

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, HashRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Layout from '../containers/Layout';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
@@ -19,7 +19,7 @@ const App = () => {
 	const initialState = useInitialState();
 	return (
 		<AppContext.Provider value={initialState}>
-			<BrowserRouter>
+			<BrowserRouter basename='/ecommerce-react'>
 				<Layout>
 					<Routes>
 						<Route exact path="/" element= {<Home/>}/>
